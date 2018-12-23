9c18504c2f0a04f333a160caed2a25ee39ea545b
Aysad Kozanoglu <aysadx@gmail.com>
2018-12-23 02:07:25 +0100

    Update composer-install.sh

--- composer-install.sh
+++ composer-install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+# Author: Aysad Kozanoglu
+# Quick Launch:
+# wget -O composer-install.sh "https://git.io/fhJaj" && sh composer-install.sh
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
+composer -v
