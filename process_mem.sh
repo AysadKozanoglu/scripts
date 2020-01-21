dadad8c5bd8cbc2594f182145cd9f74fc2bc9a21
Aysad Kozanoglu <aysadx@gmail.com>
2020-01-21 01:51:34 +0100

    Update process_mem.sh

--- process_mem.sh
+++ process_mem.sh
@@ -5,7 +5,7 @@
 # to get ram usage of nginx process
 # proc_mem.sh nginx
 
-if ps auxf | grep  $1 | grep -v grep > /dev/null ; 
+if ps auxf | grep  $1 | grep -v -E -i "grep|bash" > /dev/null;
    then 
     ps -C $1 -O rss | awk '{ count ++; sum += $2 }; END {count --; print "Number of processes =",count; print "Memory usage per process =",sum/1024/count, "MB"; print "Total memory usage =", sum/1024, "MB" ;};'
   else
