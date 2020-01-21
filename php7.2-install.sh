d7e349a654cd0d798d8496783670377264e18fa5
Aysad Kozanoglu <aysadx@gmail.com>
2020-01-21 22:48:51 +0100

    Update php7.2-install.sh

--- php7.2-install.sh
+++ php7.2-install.sh
@@ -10,19 +10,16 @@
 
 # apt-get -y remove php5* --purge
 
+PHPPACKS="php7.2 php7.2-mysql php7.2-mbstring php7.2-xml php7.2-zip php7.2-intl php7.2-gd php7.2-fpm php7.2-json php7.2-curl "
+
 echo -e "installing php7 "
 
-apt install ca-certificates apt-transport-https curl wget 
+apt install -y --yes ca-certificates apt-transport-https 
 
-wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
-# curl -fsSL https://packages.sury.org/php/apt.gpg | apt-key add -
+wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
 
-echo "deb https://packages.sury.org/php/ jessie main" > /etc/apt/sources.list.d/php.list
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
 
-apt-get update && apt-get -y install php7.2 php7.2-fpm php7.2-common php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mysql php7.2-xml
+apt update && apt install php7.2 # apt install php7.3
 
-php -m; php -v
-
-/etc/init.d/php7.2-fpm start
-
-# fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
+apt install -y --yes $PHPPACKS
