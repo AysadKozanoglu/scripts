7f88fec25c11f55e9c532f2484199969b13bef89
Aysad Kozanoglu <aysadx@gmail.com>
2020-11-05 15:20:16 +0100

    Create AES256_file_encryption.sh

---
+++ AES256_file_encryption.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#####################################################
+# Author: Aysad Kozanoglu
+#
+# OS: Debian / Ubuntu / all devivates of Debian 
+#
+# Usage: file_encrypter.sh enc|dec FILENAME
+#
+#####################################################
+
+if $(which openssl> /dev/null); 
+  then 
+     echo "openssl found, OK..."; 
+else 
+   sudo apt-install -y -q openssl; 
+fi
+
+if [ -z "$2" ] || [ -z "$1" ];
+ then
+        echo "error "; exit 1;
+fi 
+
+if [ $1 == "enc" ];
+ then
+        openssl aes-256-cbc -a -salt -in $2 -out ${2}.enc
+        rm -i ${2}
+fi
+
+if [ $1 == "dec" ];
+ then
+        openssl aes-256-cbc -d -a -in ${2}.enc -out $2
+fi
